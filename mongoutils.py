b6f548dc908065e77a5c40ce2f729b88c37a9b25
Elio Gubser <gubser@users.noreply.github.com>
2016-05-24 11:27:08 +0200

    some code to manage observatory-wide locks

--- mongoutils.py
+++ mongoutils.py
@@ -1,6 +1,6 @@
 from pymongo.collection import Collection, ReturnDocument
 
-class AutoIncrementUnknownField(Exception):
+class UnknownField(Exception):
     pass
 
 class AutoIncrementFactory:
@@ -17,7 +17,7 @@ class AutoIncrementFactory:
     def get_incrementor(self, field: str):
         # check if field exists
         if self.coll.find_one({'_id': field}) is None:
-            raise AutoIncrementUnknownField()
+            raise UnknownField()
 
         # create incrementor function and return it to the caller
         def func():
@@ -27,9 +27,54 @@ class AutoIncrementFactory:
                 return_document=ReturnDocument.BEFORE
             )
             if doc is None:
-                raise AutoIncrementUnknownField()
+                raise UnknownField()
 
             # note that this is the value before incrementing it.
             return doc['next']
 
         return func
+
+class TypelockFactory:
+    def __init__(self, coll: Collection):
+        self.coll = coll
+
+    def create(self, field: str):
+        self.coll.update_one(
+            {'_id': field},
+            {'_id': field, 'locked': False, 'owner': None},
+            upsert=True)
+
+    def delete(self, field: str):
+        self.coll.delete_one({'_id': field})
+
+    def get_locker(self, field: str, owner: str):
+        # check if field exists
+        if self.coll.find_one({'_id': field}) is None:
+            raise UnknownField()
+
+        # create lock/unlock function and return it to the caller
+        def func(lock: bool):
+            doc = self.coll.find_one_and_update(
+                {'_id': field,
+                 '$or': [
+                     {'locked': False},
+                     {'locked': True, 'owner': owner}
+                 ]},
+                {'locked': True, 'owner': owner}
+            )
+
+            if doc is not None:
+                # we got the lock
+                return True
+            else:
+                # lock failed.
+                # now determine if it does even exist?
+                doc = self.coll.find_one({'_id': field})
+                if doc is None:
+                    raise UnknownField()
+                else:
+                    # lock exists and we were not able to acquire it.
+                    return False
+
+        return func
+
