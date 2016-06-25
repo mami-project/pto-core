e6fdd0af44495bfdcfe3156252e18653702a5d5b
Elio Gubser <gubser@users.noreply.github.com>
2016-06-25 14:59:15 +0200

    createconfig for the new config structure. closes #12

--- ptocore/createconfig.py
+++ ptocore/createconfig.py
@@ -5,37 +5,46 @@ import re
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Create a new ptocore environment.'
-                                                 'Also prints the necessary mongo commands to stdout.')
-    parser.add_argument('NAME', help='Name of the new environment.')
-    parser.add_argument('OUTPUT_DIR', help='Path to directory in where the config file should be stored.')
-    parser.add_argument('--metadata', metavar='DATABASE.COLLECTION', required=True,
-                        help='Specify database and collection where the metadata resides.'
-                        'If this is omitted, the program will ask for it interactively.')
-    parser.add_argument('--ptoadmin-path', help='Path to the pto-admin directory.', required=True)
+    parser = argparse.ArgumentParser(description='create a new ptocore environment.')
 
+    parser.add_argument('NAME', help='name of the new environment.')
+    parser.add_argument('--config-file', default='-',
+                        help='path to where the configuration should be saved. '
+                             'use \'-\' for printing to standard output (default).')
+    parser.add_argument('--mongo-file', default=None,
+                        help='also create a MongoDB script and save it at the specified path. '
+                             'use \'-\' for printing to standard output. you also need to specify --mongo-metadata.')
+
+    parser.add_argument('--mongo-host', help='hostname or IP address of mongo server (default: localhost).')
+    parser.add_argument('--mongo-port', type=int, default=27017, help='port of mongo server (default 27017).')
+    parser.add_argument('--mongo-metadata', metavar='DATABASE',
+                        help='if you want to create the MongoDB script, the name of the metadata database is needed.')
 
     parser.add_argument('--ask-passwords', action='store_true', default=False,
-                        help='Ask the user for the passwords instead of creating them using urandom.')
+                        help='ask the user for the passwords instead of generating them.')
+
     parser.add_argument('--password-length', default=20, type=int,
-                        help='Define the length of generated passwords.')
+                        help='set the length of generated passwords (default 20).')
+
     parser.add_argument('--supervisor-port', type=int, default=33424)
-    parser.add_argument('--admin-host', default='localhost')
-    parser.add_argument('--admin-port', type=int, default=5000)
 
     args = parser.parse_args()
-    if not os.path.isdir(args.OUTPUT_DIR):
-        print("The given path '{}' is not a directory.".format(args.OUTPUT_DIR))
-        exit(-1)
 
     if re.fullmatch("[a-zA-Z0-9]*", args.NAME) is None:
         print("The environment name '{}' contains illegal characters. Only the characters [a-zA-Z0-9] are allowed.".format(args.NAME))
         exit(-1)
 
-    output_fn = os.path.join(args.OUTPUT_DIR, args.NAME+'.json')
-    if os.path.exists(output_fn):
-        print("I will not overwrite the existing file '{}'.".format(output_fn))
+    if args.config_file != '-' and os.path.exists(args.config_file):
+        print("I will not overwrite the existing file '{}'.".format(args.config_file))
         exit(-1)
+
+    if args.mongo_file is not None:
+        if args.mongo_metadata is None:
+            print("If you want to create the MongoDB script, please also specify --mongo-metadata.")
+            exit(-1)
+        if os.path.exists(args.mongo_file):
+            print("I will not overwrite the existing file '{}'.".format(args.mongo_file))
+            exit(-1)
 
     # prepare credentials
     password_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890-_'
@@ -44,8 +53,6 @@ def main():
         return ''.join(password_characters[c % len(password_characters)] for c in os.urandom(args.password_length))
 
     name = args.NAME
-
-    metadata_db_name, metadata_coll_name = args.metadata.split('.', 2)
 
     sensor_username = name + '-sensor'
     sensor_password = input('sensor password: ') if args.ask_passwords else generate_password()
@@ -60,7 +67,7 @@ def main():
     admin_password = input('admin password: ') if args.ask_passwords else generate_password()
 
     # build configuration
-    mongo_uri_format = 'mongodb://{u}:{p}@localhost/'
+    mongo_uri_format = 'mongodb://{u}:{p}@' + '{}:{}/'.format(args.mongo_host, args.mongo_port)
 
     doc = {
         'environment': name,
@@ -75,73 +82,81 @@ def main():
             'mongo_uri': mongo_uri_format.format(u=validator_username, p=validator_password)
         },
         'admin': {
-            'mongo_uri': mongo_uri_format.format(u=admin_username, p=admin_password),
-            'static_path': 'path/to/pto-admin',
-            'listen_host': args.admin_host,
-            'listen_port': args.admin_port
-        },
-
-        'metadata_coll': [metadata_db_name, metadata_coll_name]
+            'mongo_uri': mongo_uri_format.format(u=admin_username, p=admin_password)
+        }
     }
 
     # store configuration
-    with open(output_fn, 'wt') as fp:
-        json.dump(doc, fp, indent=2)
+    if args.config_file == '-':
+        print("\nConfiguration\n-------------")
+        print(json.dumps(doc, indent=2))
+    else:
+        with open(args.config_file, 'wt') as fp:
+            json.dump(doc, fp, indent=2)
 
-    # build commands for creating the users in mongodb
-    sensor_create_user = {
-        'user': sensor_username,
-        'pwd': sensor_password,
-        'roles': [
-            {'role': 'readWrite', 'db': name+'-core'}
-        ]
-    }
+    if args.mongo_file is not None:
+        # gather the necessary arguments for the mongo script
+        sensor_create_user = {
+            'user': sensor_username,
+            'pwd': sensor_password,
+            'roles': [
+                {'role': 'readWrite', 'db': name+'-core'}
+            ]
+        }
 
-    supervisor_create_user = {
-        'user': supervisor_username,
-        'pwd': supervisor_password,
-        'roles': [
-            {'role': 'read',        'db': metadata_db_name},
-            {'role': 'userAdmin',   'db': metadata_db_name},
-            {'role': 'dbOwner',     'db': name+'-core'},
-            {'role': 'dbOwner',     'db': name+'-temp'},
-            {'role': 'read',        'db': name+'-obs'},
-            {'role': 'userAdmin',   'db': name+'-obs'}
-        ]
-    }
+        supervisor_create_user = {
+            'user': supervisor_username,
+            'pwd': supervisor_password,
+            'roles': [
+                {'role': 'read',        'db': args.mongo_metadata},
+                {'role': 'userAdmin',   'db': args.mongo_metadata},
+                {'role': 'dbOwner',     'db': name+'-core'},
+                {'role': 'dbOwner',     'db': name+'-temp'},
+                {'role': 'read',        'db': name+'-obs'},
+                {'role': 'userAdmin',   'db': name+'-obs'}
+            ]
+        }
 
-    validator_create_user = {
-        'user': validator_username,
-        'pwd': validator_password,
-        'roles': [
-            {'role': "readWrite", 'db': metadata_db_name},
-            {'role': "readWrite", 'db': name+'-core'},
-            {'role': "readWrite", 'db': name+'-temp'},
-            {'role': "readWrite", 'db': name+'-obs'}
-        ]
-    }
+        validator_create_user = {
+            'user': validator_username,
+            'pwd': validator_password,
+            'roles': [
+                {'role': "readWrite", 'db': args.mongo_metadata},
+                {'role': "readWrite", 'db': name+'-core'},
+                {'role': "readWrite", 'db': name+'-temp'},
+                {'role': "readWrite", 'db': name+'-obs'}
+            ]
+        }
 
-    admin_create_user = {
-        'user': admin_username,
-        'pwd': admin_password,
-        'roles': [
-            {'role': 'read',        'db': metadata_db_name},
-            {'role': 'readWrite',   'db': name+'-core'},
-            {'role': 'readWrite',   'db': name+'-temp'},
-            {'role': 'read',        'db': name+'-obs'}
-        ]
-    }
+        admin_create_user = {
+            'user': admin_username,
+            'pwd': admin_password,
+            'roles': [
+                {'role': 'read',        'db': args.mongo_metadata},
+                {'role': 'readWrite',   'db': name+'-core'},
+                {'role': 'readWrite',   'db': name+'-temp'},
+                {'role': 'read',        'db': name+'-obs'}
+            ]
+        }
 
-    # and present it to the user
-    print("use admin;")
-    print("// sensor")
-    print("db.createUser(" + json.dumps(sensor_create_user, indent=2) + ");\n")
-    print("// supervisor")
-    print("db.createUser(" + json.dumps(supervisor_create_user, indent=2) + ");\n")
-    print("// validator")
-    print("db.createUser(" + json.dumps(validator_create_user, indent=2) + ");\n")
-    print("// admin")
-    print("db.createUser(" + json.dumps(admin_create_user, indent=2) + ");\n")
+        # build script
+        mongo_script = "/*\n * MongoDB script file to create users for ptocore environment '{}'\n */\n\n".format(name)
+        mongo_script += "use admin;\n\n"
+        mongo_script += "// sensor\n"
+        mongo_script += "db.createUser(" + json.dumps(sensor_create_user, indent=2) + ");\n\n"
+        mongo_script += "// supervisor\n"
+        mongo_script += "db.createUser(" + json.dumps(supervisor_create_user, indent=2) + ");\n\n"
+        mongo_script += "// validator\n"
+        mongo_script += "db.createUser(" + json.dumps(validator_create_user, indent=2) + ");\n\n"
+        mongo_script += "// admin\n"
+        mongo_script += "db.createUser(" + json.dumps(admin_create_user, indent=2) + ");\n"
+
+        if args.mongo_file == '-':
+            print("\nMongoDB script\n--------------")
+            print(mongo_script)
+        else:
+            with open(args.mongo_file, 'wt') as fp:
+                fp.write(mongo_script)
 
 
 if __name__ == "__main__":
